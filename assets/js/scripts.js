02fc6d584420fa3e32aa0a38423803990fc906ad
Andreas Steffensen <andreas.steffensen89@gmail.com>
2018-05-08 15:57:56 +0200

    fixed navbar issue on mobile

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,15 +1,7 @@
-$(document).ready(function()  {
+$('.menu-toggle').click(function() {
 
-    /* /*http://imakewebthings.com/waypoints/api/waypoint/*/
-    $('.js--section-features').waypoint(function(direction) {
-        if (direction == "down") {
-            $('nav').addClass('sticky');
-        } else {
-            $('nav').removeClass('sticky');
-        }
-    }, {
-        offset: '58px;'
-    });
-
-
-});
+    $('ul').toggleClass('opening');
+    $(this).toggleClass('open');
+  
+  })
+  
